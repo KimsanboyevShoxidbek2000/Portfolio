81c9cc65f49eb6437b88dd626ee3457481e5decd
KimsanboyevShoxidbek2000 <shox7316@gmail.com>
2021-11-08 18:28:34 +0500

    main

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+
+function open_close_menu(){
+  document.getElementById("open_menu").classList.toggle('show-menu');
+  console.log('hello');
+}
+
+function barchasi(){
+  document.getElementById('salomcha1').style.display = 'block';
+  document.getElementById('salomcha2').style.display = 'block';
+  document.getElementById('salomcha3').style.display = 'block';
+  document.getElementById('salomcha4').style.display = 'block';
+  document.getElementById('salomcha5').style.display = 'block';
+  document.getElementById('salomcha6').style.display = 'block';
+  document.getElementById('salomcha7').style.display = 'block';
+  document.getElementById('salomcha8').style.display = 'block';
+  console.log("all")
+}
+function js_project(){
+  document.getElementById('salomcha1').style.display = 'block';
+  document.getElementById('salomcha2').style.display = 'none';
+  document.getElementById('salomcha3').style.display = 'block';
+  document.getElementById('salomcha4').style.display = 'none';
+  document.getElementById('salomcha5').style.display = 'block';
+  document.getElementById('salomcha6').style.display = 'none';
+  document.getElementById('salomcha7').style.display = 'block';
+  document.getElementById('salomcha8').style.display = 'none';
+ console.log('js')
+}
+function react_project(){
+  document.getElementById('salomcha1').style.display = 'none';
+  document.getElementById('salomcha2').style.display = 'block';
+  document.getElementById('salomcha3').style.display = 'none';
+  document.getElementById('salomcha4').style.display = 'block';
+  document.getElementById('salomcha5').style.display = 'none';
+  document.getElementById('salomcha6').style.display = 'block';
+  document.getElementById('salomcha7').style.display = 'none';
+  document.getElementById('salomcha8').style.display = 'block';
+  console.log('react')
+  
+}
+  
+
+
+
